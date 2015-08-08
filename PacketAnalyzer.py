385c70d0ee9758545a5094d9b5a237aca41be0d0
nickago <ngonella@calpoly.edu>
2015-08-08 10:46:01 -0700

    Added basic IP printing

--- PacketAnalyzer.py
+++ PacketAnalyzer.py
@@ -1,28 +1,23 @@
+#!paVEnv/bin/python
+
 import socket
 import os
 import struct
 import pcapy
 import binascii
 
-def get_header(pkt):
+import paPrint
+
+def get_enet_header(pkt):
     fmt = "!6s6sH"
     HEADER_SIZE = 14
-    
-    print "Ethernet Header"
-    print "--------------------------------"
     
     #Destination Address, Source Address, Next Protocol
     (dst_mac, src_mac, etype) = struct.unpack(fmt, pkt[:HEADER_SIZE])
 
-    dst_mac = binascii.hexlify(dst_mac)
-    print "Destination MAC " + (':'.join(dst_mac[i:i+2] for i in range(0,len(dst_mac),2)))
+    paPrint.print_enet_head(dst_mac, src_mac, etype)
 
-    src_mac = binascii.hexlify(dst_mac)
-    print "Source MAC " + (':'.join(dst_mac[i:i+2] for i in range(0,len(dst_mac),2)))
-
-    print "Protocol " + str(hex(etype))
-
-    return pkt[HEADER_SIZE:], hex(etype)
+    return pkt[HEADER_SIZE:], (etype == 2048)
 
 def get_IP_header(pkt):
     fmt = "!6H4s4s"
@@ -43,13 +38,17 @@ def get_IP_header(pkt):
     protocol = ttl_proto & 0xff
 
     #format the src and dst with socket
-    src_adr = socket.ntoa(raw_src)
-    dst_adr = socket.ntoa(raw_dst)
+    src_adr = socket.inet_ntoa(raw_src)
+    dst_adr = socket.inet_ntoa(raw_dst)
 
     if protocol == 6:
         next_proto = "TCP"
     elif protocol == 17:
         next_proto = "UDP"
+    else:
+        next_proto = "Other"
+
+    paPrint.print_ip_head(src_adr, dst_adr, protocol, next_proto)
 
     return pkt[20:], next_proto
 
@@ -64,15 +63,15 @@ def get_tcp_header(pkt):
 
 def get_udp_header(pkt):
     fmt="!4H"
-    (src_port, dst_port, length, chk_sum) = struct.unpack(fmt, pkt)
+    (src_port, dst_port, length, chk_sum) = struct.unpack(fmt, pkt[:8])
 
 def main():
     #os.system("clear")
     intf = raw_input("Choose a network interface: ")
     sniff = pcapy.open_live(intf, 2048, 1, 0)
     (_,pkt) = sniff.next()
-    pkt, etype = get_header(pkt)
-    if etype is not 0x800:
+    pkt, is_IP = get_enet_header(pkt)
+    if not is_IP :
         return
 
     pkt, next_proto = get_IP_header(pkt)
